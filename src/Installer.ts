3bc4b1c3b317c68310ac6d2e30867827b74746d3
selfint <selfint@gmail.com>
2023-06-18 23:11:04 +0300

    add installer

---
+++ src/Installer.ts
@@ -0,0 +1,86 @@
+import * as path from "path";
+import { ExecException, ExecOptions, exec } from "child_process";
+
+const TREE_SITTER_VERSION = "0.20.1";
+const ELECTRON_VERSION = "22.0.0";
+
+export function getParserDir(parsersDir: string, parserName: string): string {
+    // add '-module' to parser name, since if node module == prefix string
+    // weird behavior happens
+    return path.resolve(path.join(parsersDir, `${parserName}-module`));
+}
+
+function getNodeBindingsPath(parsersDir: string, parserName: string): string {
+    // this path assumes the library has been installed via npm install --prefix, not just cloned with git
+    return path.resolve(
+        path.join(getParserDir(parsersDir, parserName), "node_modules", parserName, "bindings", "node")
+    );
+}
+
+export async function loadParser(
+    parsersDir: string,
+    parserName: string,
+    symbol: string | undefined = undefined
+): Promise<object | undefined> {
+    const nodeBindingsPath = getNodeBindingsPath(parsersDir, parserName);
+    console.log(`Loading bindings from ${nodeBindingsPath}`);
+    if (symbol === undefined) {
+        return (await import(nodeBindingsPath)) as object;
+    } else {
+        const lib = (await import(nodeBindingsPath)) as Record<string, object>;
+        return lib[symbol];
+    }
+}
+
+export async function downloadParser(
+    parsersDir: string,
+    parserName: string,
+    onData?: (data: string) => void
+): Promise<boolean> {
+    const parserDir = getParserDir(parsersDir, parserName);
+    const installCmd = `npm install --prefix ${parserDir} ${parserName}@"<=${TREE_SITTER_VERSION}"`;
+    const installOptions = { cwd: parsersDir };
+
+    const installErr = await runCmd(installCmd, installOptions, onData);
+
+    if (installErr !== undefined) {
+        console.log(installErr);
+        return false;
+    }
+
+    const rebuildCmd = `electron-rebuild -v ${ELECTRON_VERSION}`;
+    const rebuildOptions: ExecOptions = { cwd: parserDir };
+
+    const rebuildErr = await runCmd(rebuildCmd, rebuildOptions, onData);
+    if (rebuildErr !== undefined) {
+        console.log(rebuildErr);
+        return false;
+    }
+
+    return true;
+}
+
+async function runCmd(
+    cmd: string,
+    options: ExecOptions,
+    onData?: (data: string) => void
+): Promise<ExecException | undefined> {
+    console.debug(`Running with options: ${JSON.stringify(options)} cmd: ${cmd}`);
+    const err = await new Promise<ExecException | null>((resolve) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const proc = exec(cmd, options, (err, _stdout, _stderr) => {
+            if (err !== null) {
+                resolve(err);
+            } else {
+                resolve(null);
+            }
+        });
+
+        if (onData !== undefined) {
+            proc.stdout?.on("data", onData);
+            proc.stderr?.on("data", onData);
+        }
+    });
+
+    return err ?? undefined;
+}
---
+++ src/test/suite/Installer.test.ts
@@ -0,0 +1,144 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+import * as vscodeTreeSitter from "../../extension";
+
+import { afterEach, beforeEach } from "mocha";
+import { mkdtemp, rm } from "node:fs/promises";
+import { join } from "node:path";
+import { tmpdir } from "node:os";
+
+//@ts-expect-error ignore missing types
+import Rust = require("tree-sitter-rust");
+
+//@ts-expect-error ignore missing types
+import Typescript = require("tree-sitter-typescript");
+
+suite.only("Installer Test Suite", () => {
+    let tempParsersDir: string | undefined = undefined;
+    // const tempParsersDir = "/tmp/parsers";
+    beforeEach(async () => {
+        tempParsersDir = await mkdtemp(join(tmpdir(), "vscode-tree-sitter-download-test-"));
+    });
+
+    afterEach(async () => {
+        if (tempParsersDir !== undefined) {
+            await rm(tempParsersDir, { recursive: true, force: true });
+        }
+    });
+
+    test("Downloaded rust parser matches local parser", async () => {
+        if (tempParsersDir === undefined) {
+            throw new Error("temp dir was not assigned");
+        }
+
+        const testParserName = "tree-sitter-rust";
+        const downloaded = await vscodeTreeSitter.Installer.downloadParser(
+            tempParsersDir,
+            testParserName,
+            console.log
+        );
+        assert.ok(downloaded);
+
+        const dynamicRustParser = await vscodeTreeSitter.Installer.loadParser(tempParsersDir, testParserName);
+        assert.ok(dynamicRustParser !== undefined);
+        assert.deepEqual(dynamicRustParser, Rust);
+    }).timeout(120 * 1000);
+
+    test("Downloaded typescript parsers match local parsers", async () => {
+        if (tempParsersDir === undefined) {
+            throw new Error("temp dir was not assigned");
+        }
+
+        const testParserName = "tree-sitter-typescript";
+        const downloaded = await vscodeTreeSitter.Installer.downloadParser(
+            tempParsersDir,
+            testParserName,
+            console.log
+        );
+        assert.ok(downloaded);
+
+        const dynamicTypescriptParser = await vscodeTreeSitter.Installer.loadParser(
+            tempParsersDir,
+            testParserName,
+            "typescript"
+        );
+        const dynamicTSXParser = await vscodeTreeSitter.Installer.loadParser(
+            tempParsersDir,
+            testParserName,
+            "tsx"
+        );
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        assert.deepEqual(dynamicTypescriptParser, Typescript.typescript);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        assert.deepEqual(dynamicTSXParser, Typescript.tsx);
+    }).timeout(120 * 1000);
+
+    test.only("Parsers compatibility", async () => {
+        const parsers: [string, string[] | undefined][] = [
+            ["tree-sitter-rust", undefined],
+            ["tree-sitter-typescript", ["typescript", "tsx"]],
+            ["tree-sitter-go", undefined],
+            ["tree-sitter-python", undefined],
+            ["tree-sitter-javascript", undefined],
+            ["tree-sitter-html", undefined],
+            ["tree-sitter-c", undefined],
+            ["tree-sitter-cpp", undefined],
+            ["tree-sitter-c-sharp", undefined],
+            ["tree-sitter-bash", undefined],
+            // ["tree-sitter-haskell", undefined],  // uses language version 14, we use 13
+            ["tree-sitter-java", undefined],
+            ["tree-sitter-julia", undefined],
+        ];
+
+        await Promise.all(
+            parsers.map(async ([parserName, symbols]) => {
+                if (tempParsersDir === undefined) {
+                    throw new Error("temp dir was not assigned");
+                }
+
+                let number = 0;
+                const downloaded = await vscode.window.withProgress(
+                    {
+                        location: vscode.ProgressLocation.Notification,
+                        cancellable: false,
+                        title: `Installing ${parserName}`,
+                    },
+                    async (progress) => {
+                        if (tempParsersDir === undefined) {
+                            throw new Error("temp dir was not assigned");
+                        }
+                        return await vscodeTreeSitter.Installer.downloadParser(
+                            tempParsersDir,
+                            parserName,
+                            (data) => progress.report({ message: data, increment: number++ })
+                        );
+                    }
+                );
+
+                assert.ok(downloaded);
+
+                const p = new vscodeTreeSitter.Parser();
+                if (symbols === undefined) {
+                    const language = await vscodeTreeSitter.Installer.loadParser(tempParsersDir, parserName);
+                    assert.doesNotThrow(() => p.setLanguage(language));
+                } else {
+                    if (symbols.length === 0) {
+                        throw new Error("got 0 symbols");
+                    }
+
+                    for (const symbol of symbols) {
+                        const language = await vscodeTreeSitter.Installer.loadParser(
+                            tempParsersDir,
+                            parserName,
+                            symbol
+                        );
+
+                        assert.doesNotThrow(() => p.setLanguage(language));
+                    }
+                }
+            })
+        );
+    }).timeout(120 * 1000);
+});
