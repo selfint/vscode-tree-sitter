fa9583674d12fd14a5e6565df4a6bf185ba30fe3
selfint <selfint@gmail.com>
2023-06-19 14:49:30 +0300

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -50,6 +50,7 @@ async function getLanguage(parsersDir: string, languageId: string): Promise<obje
 
 export function activate(context: vscode.ExtensionContext): void {
     const parsersDir = resolve(join(context.extensionPath, "parsers"));
+    let opened = false;
 
     if (!existsSync(parsersDir)) {
         mkdirSync(parsersDir, { recursive: true });
@@ -57,6 +58,11 @@ export function activate(context: vscode.ExtensionContext): void {
 
     context.subscriptions.push(
         vscode.commands.registerCommand("vscode-tree-sitter.test", async () => {
+            if (opened) {
+                return;
+            }
+            opened = true;
+
             const editor = vscode.window.activeTextEditor;
             if (editor === undefined) {
                 return;
@@ -76,46 +82,55 @@ export function activate(context: vscode.ExtensionContext): void {
                 private languageId: string;
                 private fileTree: FileTree | undefined;
 
+                private disposables: vscode.Disposable[] = [
+                    vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
+                        console.log("a");
+                        if (event.document.uri === this.editorUri && this.fileTree !== undefined) {
+                            this.fileTree.update(event);
+                            this.eventEmitter.fire(this.uri);
+                        }
+                    }),
+                    vscode.window.onDidChangeActiveTextEditor(
+                        async (event: vscode.TextEditor | undefined) => {
+                            if (
+                                event?.document.uri === this.editorUri ||
+                                // ignore our editor
+                                event?.document.uri.toString() === this.uri.toString() ||
+                                event?.document === undefined
+                            ) {
+                                return;
+                            }
+
+                            const newLanguage =
+                                event.document.languageId === this.languageId
+                                    ? (this.fileTree?.parser.getLanguage() as object)
+                                    : await getLanguage(parsersDir, event.document.languageId);
+
+                            if (newLanguage === undefined) {
+                                return;
+                            }
+
+                            this.languageId = event.document.languageId;
+                            this.editorUri = event.document.uri;
+                            this.fileTree = FileTree.openFile(newLanguage, event.document.getText());
+
+                            this.eventEmitter.fire(this.uri);
+                        }
+                    ),
+                    vscode.window.onDidChangeVisibleTextEditors((editors) => {
+                        if (!editors.some((e) => e.document.uri.toString() === this.uri.toString())) {
+                            this.disposables.map((d) => {
+                                d.dispose();
+                            });
+                            opened = false;
+                        }
+                    }),
+                ];
+
                 constructor(document: vscode.TextDocument, language: object) {
                     this.editorUri = document.uri;
                     this.languageId = document.languageId;
                     this.fileTree = FileTree.openFile(language, document.getText());
-
-                    context.subscriptions.push(
-                        vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
-                            if (event.document.uri !== this.editorUri || this.fileTree === undefined) {
-                                return;
-                            }
-
-                            this.fileTree.update(event);
-                            this.eventEmitter.fire(this.uri);
-                        })
-                    );
-
-                    context.subscriptions.push(
-                        vscode.window.onDidChangeActiveTextEditor(
-                            async (event: vscode.TextEditor | undefined) => {
-                                if (event?.document.uri === this.editorUri || event?.document === undefined) {
-                                    return;
-                                }
-
-                                const newLanguage =
-                                    event.document.languageId === this.languageId
-                                        ? (this.fileTree?.parser.getLanguage() as object)
-                                        : await getLanguage(parsersDir, event.document.languageId);
-
-                                if (newLanguage === undefined) {
-                                    return;
-                                }
-
-                                this.languageId = event.document.languageId;
-                                this.editorUri = event.document.uri;
-                                this.fileTree = FileTree.openFile(newLanguage, event.document.getText());
-
-                                this.eventEmitter.fire(this.uri);
-                            }
-                        )
-                    );
                 }
 
                 provideTextDocumentContent(_uri: vscode.Uri, _ct: vscode.CancellationToken): string {
